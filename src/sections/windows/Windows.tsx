b703f8e1cae7cbd897670c28f485543f76ccd791
Chase Manning <me@chasemanning.co.nz>
2021-01-02 17:50:48 +1300

    :sparkles: Create Windows Section

---
+++ src/sections/windows/Windows.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledWindows = styled.div`
+  width: 15rem;
+  height: 100%;
+  background-color: purple;
+`;
+
+const Windows = () => {
+  return <StyledWindows></StyledWindows>;
+};
+
+export default Windows;
