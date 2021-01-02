fd33287f81e1729ba45e34e0dac981b020ced1fb
Chase Manning <me@chasemanning.co.nz>
2021-01-02 18:39:21 +1300

    :lipstick: Update Global font to Ubuntu

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -12,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
       margin: 0;
       padding: 0;
-      font-family: "Roboto", sans-serif;
+      font-family: 'Ubuntu', sans-serif;
       font-size: 10px;
     }
   
