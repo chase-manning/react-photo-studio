05454526178cde710457ccacc728295a9aa0e3a2
Chase Manning <me@chasemanning.co.nz>
2021-01-02 17:47:14 +1300

    :spakles: Create Documents Section

---
+++ src/sections/documents/Documents.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDocuments = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: orange;
+`;
+
+const Documents = () => {
+  return <StyledDocuments></StyledDocuments>;
+};
+
+export default Documents;
