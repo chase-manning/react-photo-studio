401473737132f94122e5a73191d99c069980db7a
Chase Manning <me@chasemanning.co.nz>
2021-01-02 17:37:05 +1300

    :sparkles: Create Options Section

---
+++ src/sections/options/Options.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledOptions = styled.div`
+  width: 100%;
+  height: 3rem;
+  background-color: yellow;
+`;
+
+const Options = () => {
+  return <StyledOptions></StyledOptions>;
+};
+
+export default Options;
