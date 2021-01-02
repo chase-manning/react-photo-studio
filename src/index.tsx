57ead77bd559fc87a717ea2dd71ef00debd96d5a
Chase Manning <me@chasemanning.co.nz>
2021-01-02 13:20:13 +1300

    :sparkles: Add Global Styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
