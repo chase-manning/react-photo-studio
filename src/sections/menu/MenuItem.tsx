78db02316367cc6fdb32b4c2a2643e88382bd971
Chase Manning <me@chasemanning.co.nz>
2021-01-02 19:38:24 +1300

    :rocket: Add Selected Color to Menu Items

--- src/sections/menu/MenuItem.tsx
+++ src/sections/menu/MenuItem.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import ContextMenu from "../../shared/ContextMenu";
 import { MenuItemType } from "./menu-items";
 
+type StyledMenuItemProps = {
+  open: boolean;
+};
+
 const StyledMenuItem = styled.div`
   position: relative;
   height: 100%;
@@ -13,9 +17,12 @@ const StyledMenuItem = styled.div`
   align-items: center;
   cursor: pointer;
 
+  background-color: ${(props: StyledMenuItemProps) =>
+    props.open ? "var(--selected)" : "var(--panel)"};
   border: solid 1px var(--panel);
   :hover {
-    background-color: var(--hover-bg);
+    background-color: ${(props: StyledMenuItemProps) =>
+      props.open ? "var(--selected)" : "var(--hover-bg)"};
     border: solid 1px var(--hover-border);
   }
 `;
@@ -28,7 +35,7 @@ const MenuItem = (props: Props) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <StyledMenuItem onClick={() => setOpen(true)}>
+    <StyledMenuItem onClick={() => setOpen(true)} open={open}>
       {props.menuItem.name}
       <ContextMenu open={open} />
     </StyledMenuItem>
