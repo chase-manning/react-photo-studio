22808750f856091cb44ab36416a50c4962dd50b3
Chase Manning <me@chasemanning.co.nz>
2021-01-02 17:37:14 +1300

    :spakles: Create Tools Section

---
+++ src/sections/tools/Tools.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTools = styled.div`
+  height: 100%;
+  width: 3rem;
+  background-color: red;
+`;
+
+const Tools = () => {
+  return <StyledTools></StyledTools>;
+};
+
+export default Tools;
