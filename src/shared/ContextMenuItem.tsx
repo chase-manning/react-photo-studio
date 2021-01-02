b9f7bb6154bb88192ef907d0f832511d290e500b
Chase Manning <me@chasemanning.co.nz>
2021-01-03 11:18:16 +1300

    :sparkles: Create Context Menu Item Component

---
+++ src/shared/ContextMenuItem.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { MenuItemType } from "../sections/menu/menu-items";
+import { ReactComponent as ArrowIcon } from "../assets/svgs/navigation/triangle-right.svg";
+
+const StyledContextMenuItem = styled.button`
+  padding: 0.2rem 0.4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  :hover {
+    background-color: var(--context-hover);
+  }
+`;
+
+const ItemText = styled.div`
+  color: var(--text);
+  font-size: 1.4rem;
+  white-space: nowrap;
+`;
+
+const Arrow = styled(ArrowIcon)`
+  height: 1rem;
+  fill: var(--text);
+  margin-left: 2rem;
+`;
+
+type Props = {
+  menuItem: MenuItemType;
+};
+
+const ContextMenuItem = (props: Props) => {
+  return (
+    <StyledContextMenuItem key={props.menuItem.name}>
+      <ItemText>{props.menuItem.name}</ItemText>
+      {props.menuItem.items.length > 0 && <Arrow />}
+    </StyledContextMenuItem>
+  );
+};
+
+export default ContextMenuItem;
