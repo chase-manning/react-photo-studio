0374296bd543cbed37ea3e5953865337cce899e9
Chase Manning <me@chasemanning.co.nz>
2021-01-02 13:19:43 +1300

    :rocket: Create Base of App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
 
 const App = () => {
-  return <div>Meow</div>;
+  return <StyledApp>meow</StyledApp>;
 };
 
 export default App;
