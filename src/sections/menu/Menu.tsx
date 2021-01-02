0881d587a52d83673d5f15baf32df4f277e6a9c3
Chase Manning <me@chasemanning.co.nz>
2021-01-02 17:36:56 +1300

    :sparkles: Create Menu Section

---
+++ src/sections/menu/Menu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledMenu = styled.div`
+  width: 100%;
+  height: 3rem;
+  background-color: green;
+`;
+
+const Menu = () => {
+  return <StyledMenu></StyledMenu>;
+};
+
+export default Menu;
