c123f209405f3541289bd81bc4f009623064ecc3
Chase Manning <me@chasemanning.co.nz>
2021-01-02 18:12:41 +1300

    :lipstick: Set Color of Menu Section

--- src/sections/menu/Menu.tsx
+++ src/sections/menu/Menu.tsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 
 const StyledMenu = styled.div`
   width: 100%;
-  height: 4rem;
-  background-color: green;
+  height: 3rem;
+  background-color: var(--panel);
+  border-bottom: solid 1px var(--border);
+  padding: 5px;
 `;
 
 const Menu = () => {
