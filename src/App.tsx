e89d086d85e2d3219b3ac5517bf2a75b2a8e09b2
Chase Manning <me@chasemanning.co.nz>
2021-01-02 17:37:40 +1300

    :rocket: Add App Scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Menu from "./sections/menu/Menu";
+import Options from "./sections/options/Options";
+import Tools from "./sections/tools/Tools";
 
 const StyledApp = styled.div`
   position: fixed;
@@ -7,10 +10,28 @@ const StyledApp = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Horizontal = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const App = () => {
-  return <StyledApp>meow</StyledApp>;
+  return (
+    <StyledApp>
+      <Menu />
+      <Options />
+      <Horizontal>
+        <Tools />
+      </Horizontal>
+    </StyledApp>
+  );
 };
 
 export default App;
