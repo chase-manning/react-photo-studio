24d176d9220a8951710b7c1bdc1af617cad4b91b
Chase Manning <me@chasemanning.co.nz>
2021-01-04 22:14:08 +1300

    :rocket: Add Logging to 3D Schema

--- src/sections/menu/schema/three-d.ts
+++ src/sections/menu/schema/three-d.ts
@@ -1,7 +1,8 @@
+import { FeatureRequest } from "../../../services/AnalyticsService";
 import { ItemType } from "./menu-items";
 
 export const threeD: ItemType = {
   name: "3D",
   itemSets: [],
-  action: () => console.log("meow"),
+  action: () => FeatureRequest("Menu/3D"),
 };
