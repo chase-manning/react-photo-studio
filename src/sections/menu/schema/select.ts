214be10bdeb2eb6eae6f22e92065951d40777adb
Chase Manning <me@chasemanning.co.nz>
2021-01-04 22:13:00 +1300

    :rocket: Add Logging to Select Schema

--- src/sections/menu/schema/select.ts
+++ src/sections/menu/schema/select.ts
@@ -1,7 +1,8 @@
+import { FeatureRequest } from "../../../services/AnalyticsService";
 import { ItemType } from "./menu-items";
 
 export const select: ItemType = {
   name: "Select",
   itemSets: [],
-  action: () => console.log("meow"),
+  action: () => FeatureRequest("Menu/Select"),
 };
