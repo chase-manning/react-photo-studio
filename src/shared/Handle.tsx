f28f5b073caafc56463bd082884832cefcbcd97e
Chase Manning <me@chasemanning.co.nz>
2021-01-04 22:26:41 +1300

    :lipstick: Reduce Handle Line Height

--- src/shared/Handle.tsx
+++ src/shared/Handle.tsx
@@ -3,21 +3,21 @@ import styled from "styled-components";
 
 const StyledHandle = styled.div`
   width: 100%;
-  height: 1.2rem;
+  height: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const Lines = styled.div`
-  width: 60%;
+  height: 50%;
   display: flex;
-  justify-content: space-between;
 `;
 
 const Line = styled.div`
   height: 100%;
   width: 0.1rem;
+  margin-right: 0.1rem;
   background-color: var(--expandable);
 `;
 
