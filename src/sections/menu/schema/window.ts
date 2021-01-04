8308b97d038991f7343dcd5b5198c0e334fb3c02
Chase Manning <me@chasemanning.co.nz>
2021-01-04 22:14:51 +1300

    :rocket: Add Logging to Window Schema

--- src/sections/menu/schema/window.ts
+++ src/sections/menu/schema/window.ts
@@ -1,7 +1,8 @@
+import { FeatureRequest } from "../../../services/AnalyticsService";
 import { ItemType } from "./menu-items";
 
 export const window: ItemType = {
   name: "Window",
   itemSets: [],
-  action: () => console.log("meow"),
+  action: () => FeatureRequest("Menu/Window"),
 };
