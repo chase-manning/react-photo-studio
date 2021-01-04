1413d2617a4d4b79566aedec822b2ba05479701f
Chase Manning <me@chasemanning.co.nz>
2021-01-04 17:33:09 +1300

    :lipstick: Add Background Color to Panel

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const StyledApp = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background-color: var(--panel);
 `;
 
 const Horizontal = styled.div`
