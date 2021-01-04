a3e6996121dcdb09aa27e0af6654819d5cfd5b9e
Chase Manning <me@chasemanning.co.nz>
2021-01-04 22:16:04 +1300

    :fire: Remove Unused Import

--- src/sections/tools/Tools.tsx
+++ src/sections/tools/Tools.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { FeatureRequest } from "../../services/AnalyticsService";
 import Expandable from "../../shared/Expandable";
