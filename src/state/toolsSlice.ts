f9a609fb9a2415fe09df82275c1b7d8b53603ace
Chase Manning <me@chasemanning.co.nz>
2021-01-06 11:57:22 +1300

    :sparkles: Create Tools Schema State

--- src/state/toolsSlice.ts
+++ src/state/toolsSlice.ts
@@ -1,29 +1,95 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { tools, ToolCollection } from "../sections/tools/Tools";
 import { RootState } from "./store";
 
+export enum ToolOption {
+  MOVE,
+  ARTBOARD,
+  RECTANGULAR_MARQUEE,
+  ELLIPTICAL_MARQUEE,
+  SINGLE_ROW_MARQUEE,
+  SINGLE_COLUMN_MARQUEE,
+  LASSO,
+  POYGONAL_LASSO,
+  MAGNETIC_LASSO,
+  OBJECT_SELECTION,
+  QUICK_SELECTION,
+  MAGIC_WAND,
+  CROP,
+  PERSPECTIVE_CROP,
+  SLICE,
+  SLICE_SELECT,
+  FRAME,
+  EYEDROPPER,
+  THREE_D_MATERIAL_EYEDROPPER,
+  COLOR_SAMPLER,
+  RULER,
+  NOTE,
+  RED_EYE,
+  BRUSH,
+  PENCIL,
+  COLOR_REPLACEMENT,
+  MIXER_BRUSH,
+  CLONE_STAMP,
+  PATTERN_STAMP,
+  HISTORY_BRUSH,
+  ART_HISTORY_BRUSH,
+  ERASER,
+  BACKGROUND_ERASER,
+  MAGIC_ERASER,
+  GRADIENT,
+  PAINT_BUCKET,
+  THREE_D_MATERIAL_DROP,
+  BLUR,
+  SHARPEN,
+  SMUDGE,
+  DODGE,
+  BURN,
+  SPONGE,
+  PEN,
+  FREEFORM_PEN,
+  CURVATURE_PEN,
+  ADD_ANCHOR_POINT,
+  DELETE_ANCHOR_POINT,
+  CONVERT_POINT,
+  HORIZONTAL_TYPE,
+  VERTICAL_TYPE,
+  VERTICAL_TYPE_MASK,
+  HORIZONTAL_TYPE_MASK,
+  PATH_SELECTION,
+  DIRECT_SELECTION,
+  RECTANGLE,
+  ROUNDED_RECTANGLE,
+  ELLIPSE,
+  POLYGON,
+  LINE,
+  CUSTOM_SHAPE,
+  HAND,
+  ROTATE_VIEW,
+  ZOOM,
+  EDIT_TOOLBAR,
+}
+
 export interface SettingsState {
-  test: boolean;
+  schema: ToolCollection[];
 }
 
 const initialState: SettingsState = {
-  test: true,
+  schema: tools,
 };
 
 export const toolsSlice = createSlice({
   name: "tools",
   initialState,
   reducers: {
-    toggleRandomReminders: (state) => {
-      state.test = true;
-    },
-    setFrequencyMinutesMin: (state, action: PayloadAction<boolean>) => {
-      state.test = action.payload;
+    setActiveTool: (state, action: PayloadAction<ToolOption>) => {
+      state.schema = state.schema;
     },
   },
 });
 
-export const { toggleRandomReminders } = toolsSlice.actions;
+export const { setActiveTool } = toolsSlice.actions;
 
-export const selectSettings = (state: RootState) => state.tools.test;
+export const selectTools = (state: RootState) => state.tools.schema;
 
 export default toolsSlice.reducer;
