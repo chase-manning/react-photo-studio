4365cd55e0925a5d0913f882004a0398a6a61394
Chase Manning <me@chasemanning.co.nz>
2021-01-06 16:39:02 +1300

    :rocket: Add Rectangular Marquee Icon to Tools

--- src/sections/tools/ToolIcon.tsx
+++ src/sections/tools/ToolIcon.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { ToolOption } from "./toolSchema";
 import move from "../../assets/svgs/tools/move.svg";
+import rectangularMarquee from "../../assets/svgs/tools/rectangular-marquee.svg";
 
 const StyledToolIcon = styled.img`
   height: 100%;
@@ -11,8 +12,10 @@ const GetIcon = (option: ToolOption): string => {
   switch (option) {
     case ToolOption.MOVE:
       return move;
+    case ToolOption.RECTANGULAR_MARQUEE:
+      return rectangularMarquee;
     default:
-      return move;
+      return "";
   }
 };
 
