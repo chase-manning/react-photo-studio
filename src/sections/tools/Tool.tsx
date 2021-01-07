245441fdf56306049b37f1610ae569c3da37e400
Chase Manning <me@chasemanning.co.nz>
2021-01-07 22:01:43 +1300

    :lipstick: Increase Tool Padding

--- src/sections/tools/Tool.tsx
+++ src/sections/tools/Tool.tsx
@@ -26,7 +26,7 @@ const ToolButton = styled.button`
   align-items: center;
   justify-content: center;
   border-radius: 0.2rem;
-  padding: 0.3rem;
+  padding: 0.4rem;
 
   background-color: ${(props: ToolButtonProps) =>
     props.open ? "var(--selected)" : "var(--panel)"};
