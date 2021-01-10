a6497e5395e927bec0b754ff139f66406d654456
Chase Manning <me@chasemanning.co.nz>
2021-01-10 17:25:41 +1300

    :art: change to using react version of hotkeys

--- src/orchestration/Shortcuts.tsx
+++ src/orchestration/Shortcuts.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import hotkeys from "hotkeys-js";
+import React, { useEffect } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { useDispatch, useSelector } from "react-redux";
+import { ShortcutType } from "../state/shortcutsSchema";
 import { selectShortcuts } from "../state/shortcutsSlice";
 
 const Shortcuts = () => {
   const dispatch = useDispatch();
   const shortcuts = useSelector(selectShortcuts);
+
+  useHotkeys("command+r", () => {
+    console.log("meowmeow");
+    return false;
+  });
+
+  // shortcuts.forEach((shortcut: ShortcutType) => {
+  //   hotkeys("command+r", (event) => {
+  //     if (!event.repeat) {
+  //       // console.log("key");
+  //       alert("meow");
+  //     }
+  //     return false;
+  //   });
+  // });
 
   return null;
 };
