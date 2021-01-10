e753102d8e18c6d4e899abdcb4f377bb8218ef90
Chase Manning <me@chasemanning.co.nz>
2021-01-10 22:31:41 +1300

    :art: change shortcuts to be per os and reducers

--- src/orchestration/Shortcuts.tsx
+++ src/orchestration/Shortcuts.tsx
@@ -1,23 +1,26 @@
 import { useHotkeys } from "react-hotkeys-hook";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ShortcutType } from "../state/shortcutsSchema";
 import { selectShortcuts } from "../state/shortcutsSlice";
+import { isMacOs } from "react-device-detect";
 
 const Shortcuts = () => {
+  const dispatch = useDispatch();
   const shortcuts = useSelector(selectShortcuts);
 
   const shortcutKeys = shortcuts.reduce(
-    (a: string, b: ShortcutType) => a + b.shortcut + ",",
+    (a: string, b: ShortcutType) =>
+      a + isMacOs ? b.macShortcut : b.windowsShortcut + ",",
     ""
   );
 
   useHotkeys(shortcutKeys, (event: KeyboardEvent, handler) => {
     event.preventDefault();
     if (event.repeat) return false;
-    const shortcut = shortcuts.filter(
-      (shortcut: ShortcutType) => shortcut.shortcut === handler.key
+    const shortcut = shortcuts.filter((shortcut: ShortcutType) =>
+      isMacOs ? shortcut.macShortcut : shortcut.windowsShortcut === handler.key
     );
-    if (shortcut.length > 0) shortcut[0].action();
+    if (shortcut.length > 0) dispatch(shortcut[0].action());
   });
 
   return null;
--- src/state/shortcutsSchema.ts
+++ src/state/shortcutsSchema.ts
@@ -1,6 +1,15 @@
+import { swapColors } from "./toolsSlice";
+
 export type ShortcutType = {
-  shortcut: string;
+  macShortcut: string;
+  windowsShortcut: string;
   action: () => void;
 };
 
-export const shortcutSchema: ShortcutType[] = [];
+export const shortcutSchema: ShortcutType[] = [
+  {
+    macShortcut: "command+r",
+    windowsShortcut: "ctrl+n",
+    action: () => swapColors(),
+  },
+];
