d14596b9d152a0abdd9511f4151d55fa48dc49c6
Chase Manning <me@chasemanning.co.nz>
2021-01-10 20:56:47 +1300

    :fire: remove placeholder shortcuts

--- src/state/shortcutsSchema.ts
+++ src/state/shortcutsSchema.ts
@@ -3,13 +3,4 @@ export type ShortcutType = {
   action: () => void;
 };
 
-export const shortcutSchema: ShortcutType[] = [
-  {
-    shortcut: "command+r",
-    action: () => alert("meow"),
-  },
-  {
-    shortcut: "command+t",
-    action: () => alert("meow2"),
-  },
-];
+export const shortcutSchema: ShortcutType[] = [];
