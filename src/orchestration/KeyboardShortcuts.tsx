714e0b6dabf9168d42572c5f214f89dfd1b754aa
Chase Manning <me@chasemanning.co.nz>
2021-01-10 16:22:47 +1300

    :sparkles: create keyboard shortcuts component

---
+++ src/orchestration/KeyboardShortcuts.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+const KeyboardShortcuts = () => {
+  const dispatch = useDispatch();
+
+  return null;
+};
+
+export default KeyboardShortcuts;
