d3662a3036661d7e433e541b4c0bb55ee61de0e8
Chase Manning <me@chasemanning.co.nz>
2021-01-11 21:03:43 +1300

    :art: change shortcuts to use shortcut service

--- src/orchestration/Shortcuts.tsx
+++ src/orchestration/Shortcuts.tsx
@@ -2,23 +2,22 @@ import { useHotkeys } from "react-hotkeys-hook";
 import { useDispatch, useSelector } from "react-redux";
 import { ShortcutType } from "../state/shortcutsSchema";
 import { selectShortcuts } from "../state/shortcutsSlice";
-import { isMacOs } from "react-device-detect";
+import { DeviceShortcut } from "../services/ShortcutService";
 
 const Shortcuts = () => {
   const dispatch = useDispatch();
   const shortcuts = useSelector(selectShortcuts);
 
   const shortcutKeys = shortcuts.reduce(
-    (a: string, b: ShortcutType) =>
-      a + isMacOs ? b.macShortcut : b.windowsShortcut + ",",
+    (a: string, b: ShortcutType) => a + DeviceShortcut(b) + ",",
     ""
   );
 
   useHotkeys(shortcutKeys, (event: KeyboardEvent, handler) => {
     event.preventDefault();
     if (event.repeat) return false;
-    const shortcut = shortcuts.filter((shortcut: ShortcutType) =>
-      isMacOs ? shortcut.macShortcut : shortcut.windowsShortcut === handler.key
+    const shortcut = shortcuts.filter(
+      (shortcut: ShortcutType) => DeviceShortcut(shortcut) === handler.key
     );
     if (shortcut.length > 0) dispatch(shortcut[0].action());
   });
