7db770c309db4dd5148c3acd5f9c09b65579156d
Chase Manning <me@chasemanning.co.nz>
2021-01-16 14:23:35 +1300

    :sparkles: add sideways option to handle

--- src/shared/Handle.tsx
+++ src/shared/Handle.tsx
@@ -3,39 +3,49 @@ import styled from "styled-components";
 import { FeatureRequest } from "../services/AnalyticsService";
 
 const StyledHandle = styled.button`
-  width: 100%;
-  height: 1rem;
+  width: ${(props: Props) => (props.sideways ? "1rem" : "100%")};
+  height: ${(props: Props) => (props.sideways ? "100%" : "1rem")};
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const Lines = styled.div`
-  height: 50%;
+  width: ${(props: Props) => (props.sideways ? "50%" : "auto")};
+  height: ${(props: Props) => (props.sideways ? "auto" : "50%")};
   display: flex;
+  flex-direction: ${(props: Props) => (props.sideways ? "column" : "row")};
 `;
 
 const Line = styled.div`
-  height: 100%;
-  width: 0.1rem;
-  margin-right: 0.1rem;
+  width: ${(props: Props) => (props.sideways ? "100%" : "0.1rem")};
+  height: ${(props: Props) => (props.sideways ? "0.1rem" : "100%")};
+  margin-top: ${(props: Props) => (props.sideways ? "0.1rem" : "0")};
+  margin-right: ${(props: Props) => (props.sideways ? "0" : "0.1rem")};
   background-color: var(--expandable);
 `;
 
-const Handle = () => {
+type Props = {
+  sideways?: boolean;
+};
+
+const Handle = (props: Props) => {
   return (
-    <StyledHandle onClick={() => FeatureRequest("Tools/Handle")}>
-      <Lines>
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
-        <Line />
+    <StyledHandle
+      onClick={() => FeatureRequest("Tools/Handle")}
+      sideways={props.sideways}
+    >
+      <Lines sideways={props.sideways}>
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
+        <Line sideways={props.sideways} />
       </Lines>
     </StyledHandle>
   );
