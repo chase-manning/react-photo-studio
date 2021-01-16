0d225bf6923a8a25bdc17876b79d5f529b6ba05b
Chase Manning <me@chasemanning.co.nz>
2021-01-16 17:52:43 +1300

    :rocket: add brush option to options

--- src/sections/options/Option.tsx
+++ src/sections/options/Option.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import PercentSelector from "../../shared/PercentSelector";
 import { OptionOption, OptionType } from "../../state/toolSchema";
+import BrushOption from "./BrushOption";
 import Presets from "./Presets";
 
 const StyledOption = styled.div`
@@ -20,6 +21,7 @@ const Option = (props: Props) => {
   return (
     <StyledOption>
       {props.option.option === OptionOption.PRESET && <Presets />}
+      {props.option.option === OptionOption.BRUSH && <BrushOption />}
       {props.option.option === OptionOption.OPACITY && (
         <PercentSelector label={"Opacity"} value={props.option.value!} />
       )}
