ca584731fb309d7b57eae66af2e32b8bf38d692a
Chase Manning <me@chasemanning.co.nz>
2021-01-16 14:03:09 +1300

    :rocket: show tool options

--- src/sections/options/Options.tsx
+++ src/sections/options/Options.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { OptionType } from "../../state/toolSchema";
+import { selectOptions } from "../../state/toolsSlice";
+import Option from "./Option";
 
 const StyledOptions = styled.div`
   width: 100%;
@@ -8,7 +12,15 @@ const StyledOptions = styled.div`
 `;
 
 const Options = () => {
-  return <StyledOptions></StyledOptions>;
+  const options = useSelector(selectOptions);
+
+  return (
+    <StyledOptions>
+      {options.map((option: OptionType) => (
+        <Option option={option} />
+      ))}
+    </StyledOptions>
+  );
 };
 
 export default Options;
