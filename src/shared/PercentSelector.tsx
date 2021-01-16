4b09c45246aebb99b3a1ade8d176faa48f91e68c
Chase Manning <me@chasemanning.co.nz>
2021-01-16 13:50:19 +1300

    :sparkles: create percent selector component

---
+++ src/shared/PercentSelector.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPercentSelector = styled.div`
+  display: flex;
+`;
+
+const Label = styled.p`
+  color: var(--label);
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  color: var(--input-text);
+`;
+
+const Button = styled.button`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--hover-bg);
+`;
+
+const Icon = styled.div`
+  transform: rotate(90deg);
+  color: var(--icon);
+`;
+
+type Props = {
+  label: string;
+  value: number;
+};
+
+const PercentSelector = (props: Props) => {
+  return (
+    <StyledPercentSelector>
+      <Label>{props.label}</Label>
+      <InputContainer>
+        <Input>{props.value}</Input>
+        <Button>
+          <Icon>{">"}</Icon>
+        </Button>
+      </InputContainer>
+    </StyledPercentSelector>
+  );
+};
+
+export default PercentSelector;
