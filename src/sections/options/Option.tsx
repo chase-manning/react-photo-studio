0ba30d21bad5cda81892447f2ff44d1f7f501215
Chase Manning <me@chasemanning.co.nz>
2021-01-16 20:34:13 +1300

    :rocket: add brush settings option

--- src/sections/options/Option.tsx
+++ src/sections/options/Option.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import PercentSelector from "../../shared/PercentSelector";
 import { OptionOption, OptionType } from "../../state/toolSchema";
+import Button from "../../styles/Button";
 import BrushOption from "./BrushOption";
 import Presets from "./Presets";
+import brushSettings from "../../assets/options/brush-settings.svg";
 
 const StyledOption = styled.div`
   display: flex;
@@ -11,6 +13,10 @@ const StyledOption = styled.div`
   align-items: center;
   justify-content: center;
   padding: 0 0.4rem;
+`;
+
+const Image = styled.img`
+  height: 100%;
 `;
 
 type Props = {
@@ -23,6 +29,11 @@ const Option = (props: Props) => {
       {props.option.option === OptionOption.PRESET && <Presets />}
       {props.option.option === OptionOption.BRUSH && (
         <BrushOption option={props.option} />
+      )}
+      {props.option.option === OptionOption.SETTINGS && (
+        <Button selected={props.option.enabled!!} width={"auto"}>
+          <Image src={brushSettings} />
+        </Button>
       )}
       {props.option.option === OptionOption.OPACITY && (
         <PercentSelector label={"Opacity"} value={props.option.value!} />
