1ad8a0dae6d76d8299f455691c2393bb42aa3f69
Chase Manning <me@chasemanning.co.nz>
2021-01-16 15:39:44 +1300

    :lipstick: add padding between options

--- src/sections/options/Option.tsx
+++ src/sections/options/Option.tsx
@@ -3,7 +3,13 @@ import styled from "styled-components";
 import PercentSelector from "../../shared/PercentSelector";
 import { OptionOption, OptionType } from "../../state/toolSchema";
 
-const StyledOption = styled.div``;
+const StyledOption = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 0 0.6rem;
+`;
 
 type Props = {
   option: OptionType;
