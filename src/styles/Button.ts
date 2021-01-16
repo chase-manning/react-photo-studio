c37878aab485cc678ce57556ca656ea4114a55eb
Chase Manning <me@chasemanning.co.nz>
2021-01-16 19:05:50 +1300

    :sparkles: create button styled component

---
+++ src/styles/Button.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+type Props = {
+  selected: boolean;
+  height?: string;
+  width?: string;
+};
+
+const Button = styled.button`
+  position: relative;
+  height: ${(props: Props) => props.height || "100%"};
+  width: ${(props: Props) => props.width || "100%"};
+  color: var(--text);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.2rem;
+  padding: 0.4rem;
+
+  background-color: ${(props: Props) =>
+    props.selected ? "var(--selected)" : "var(--panel)"};
+  border: solid 1px
+    ${(props: Props) =>
+      props.selected ? "var(--hover-border)" : "var(--panel)"};
+  :hover {
+    background-color: ${(props: Props) =>
+      props.selected ? "var(--selected)" : "var(--hover-bg)"};
+    border: solid 1px var(--hover-border);
+  }
+`;
+
+export default Button;
