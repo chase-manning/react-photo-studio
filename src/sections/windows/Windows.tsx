f36d09603519c2503a71cf54291b842659ffc508
Chase Manning <me@chasemanning.co.nz>
2021-01-18 00:15:31 +1300

    :rocket: add windows to windows

--- src/sections/windows/Windows.tsx
+++ src/sections/windows/Windows.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { FeatureRequest } from "../../services/AnalyticsService";
 import Expandable from "../../shared/Expandable";
+import Tabs, { TabType } from "../../shared/Tabs";
+import windows from "./windowSchema";
+import Window from "./Window";
 
 const StyledWindows = styled.div`
   width: 33rem;
@@ -18,6 +21,12 @@ const Windows = () => {
         right={true}
         toggle={() => FeatureRequest("Windows/Expandable")}
       />
+      {windows.map((window: string[]) => {
+        const tabs: TabType[] = [];
+        window.forEach((label: string) =>
+          tabs.push({ label: label, content: <Window window={label} /> })
+        );
+      })}
     </StyledWindows>
   );
 };
