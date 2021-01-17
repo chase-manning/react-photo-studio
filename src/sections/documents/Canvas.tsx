54a5d2b2471827dc6ef07420e6fec25ccec50ded
Chase Manning <me@chasemanning.co.nz>
2021-01-17 16:03:19 +1300

    :sparkles: create canvas component

---
+++ src/sections/documents/Canvas.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledCanvas = styled.div`
+  height: 40rem;
+  width: 60rem;
+  background-color: white;
+`;
+
+const Canvas = () => {
+  return <StyledCanvas></StyledCanvas>;
+};
+
+export default Canvas;
