938183bf03cc65d30ba92c6048959255afcbcfa5
Chase Manning <me@chasemanning.co.nz>
2021-01-17 23:27:24 +1300

    :rocket: add expandable to windows

--- src/sections/windows/Windows.tsx
+++ src/sections/windows/Windows.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { FeatureRequest } from "../../services/AnalyticsService";
+import Expandable from "../../shared/Expandable";
 
 const StyledWindows = styled.div`
   width: 33rem;
   height: 100%;
-  background-color: yellow;
+  background-color: var(--panel);
 `;
 
 const Windows = () => {
-  return <StyledWindows></StyledWindows>;
+  return (
+    <StyledWindows>
+      <Expandable
+        open={true}
+        right={true}
+        toggle={() => FeatureRequest("Windows/Expandable")}
+      />
+    </StyledWindows>
+  );
 };
 
 export default Windows;
