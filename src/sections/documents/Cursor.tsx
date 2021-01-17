b1c5945b73e03e571adb23e3e54303e7b41e733b
Chase Manning <me@chasemanning.co.nz>
2021-01-17 19:24:26 +1300

    :rocket: add cursor tracking

--- src/sections/documents/Cursor.tsx
+++ src/sections/documents/Cursor.tsx
@@ -4,18 +4,26 @@ import styled from "styled-components";
 import { Position, selectCursorPosition } from "../../state/cursorSlice";
 
 const StyledCursor = styled.div`
+  top: 0;
+  left: 0;
   position: absolute;
-  top: ${(props: Position) => props.y};
-  left: ${(props: Position) => props.x};
   height: 10px;
   width: 10px;
   background-color: red;
 `;
 
-const Cursor = () => {
-  const cursorPosition = useSelector(selectCursorPosition);
+type Props = {
+  documentPosition: Position;
+};
 
-  return <StyledCursor x={cursorPosition.x} y={cursorPosition.y} />;
+const Cursor = (props: Props) => {
+  const cursorPosition = useSelector(selectCursorPosition);
+  const x = cursorPosition.x - props.documentPosition.x + "px";
+  const y = cursorPosition.y - props.documentPosition.y + "px";
+  //   const x = cursorPosition.x + "px";
+  //   const y = cursorPosition.y + "px";
+
+  return <StyledCursor style={{ transform: `translate(${x},${y})` }} />;
 };
 
 export default Cursor;
--- src/sections/documents/Document.tsx
+++ src/sections/documents/Document.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { setCursorPosition } from "../../state/cursorSlice";
 import Canvas from "./Canvas";
 import Cursor from "./Cursor";
 
@@ -11,13 +13,31 @@ const StyledDocument = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: none;
 `;
 
 const Document = () => {
+  const dispatch = useDispatch();
+  const document = useRef<HTMLDivElement>(null);
+  const [showCursor, setShowCursor] = useState(false);
+
+  const documentRect = document.current?.getBoundingClientRect();
+  const documentPosition = {
+    x: documentRect?.x || 0,
+    y: documentRect?.y || 0,
+  };
+
   return (
-    <StyledDocument>
+    <StyledDocument
+      ref={document}
+      onMouseEnter={() => setShowCursor(true)}
+      onMouseLeave={() => setShowCursor(false)}
+      onMouseMove={(e) =>
+        dispatch(setCursorPosition({ x: e.clientX, y: e.clientY }))
+      }
+    >
       <Canvas />
-      <Cursor />
+      {showCursor && <Cursor documentPosition={documentPosition} />}
     </StyledDocument>
   );
 };
