385047734b9c4e9eebe8100e88b56eaed0877165
Chase Manning <me@chasemanning.co.nz>
2021-01-17 15:59:22 +1300

    :sparkles: create document component

---
+++ src/sections/documents/Document.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDocument = styled.div`
+  flex: 1;
+  height: 100%;
+  background-color: var(--bg);
+`;
+
+const Document = () => {
+  return <StyledDocument></StyledDocument>;
+};
+
+export default Document;
