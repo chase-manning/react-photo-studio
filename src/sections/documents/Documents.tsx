2ae02a2ee669a250ce3b805220803d38e5d0b379
Chase Manning <me@chasemanning.co.nz>
2021-01-17 15:59:32 +1300

    :rocket: create scaffolding of documents section

--- src/sections/documents/Documents.tsx
+++ src/sections/documents/Documents.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import Document from "./Document";
+import DocumentFooter from "./DocumentFooter";
+import DocumentTabs from "./DocumentTabs";
 
 const StyledDocuments = styled.div`
   flex: 1;
   height: 100%;
-  background-color: blue;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  border: solid 1px var(--border);
+`;
+
+const Center = styled.div`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Sidebar = styled.div`
+  height: 100%;
+  width: 2rem;
+  background-color: var(--sidebar);
 `;
 
 const Documents = () => {
-  return <StyledDocuments></StyledDocuments>;
+  return (
+    <StyledDocuments>
+      <DocumentTabs />
+      <Center>
+        <Document />
+        <Sidebar />
+      </Center>
+      <DocumentFooter />
+    </StyledDocuments>
+  );
 };
 
 export default Documents;
