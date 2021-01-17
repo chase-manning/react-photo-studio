f7dc6b6c63a473c95a3ca05eb51e485c303b0c06
Chase Manning <me@chasemanning.co.nz>
2021-01-17 17:46:01 +1300

    :rocket: add zoom and details to document footer

--- src/sections/documents/DocumentFooter.tsx
+++ src/sections/documents/DocumentFooter.tsx
@@ -8,8 +8,51 @@ const StyledDocumentFooter = styled.div`
   background-color: var(--sidebar);
 `;
 
+const Zoom = styled.div`
+  height: 100%;
+  width: 7rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--hover-bg);
+  border-top: solid 1px var(--expandable);
+  border-right: solid 1px var(--expandable);
+  color: var(--input-text);
+`;
+
+const DetailsContainer = styled.div`
+  height: 100%;
+  width: 15rem;
+  display: flex;
+  align-items: center;
+  background-color: var(--panel);
+  border-top: solid 1px var(--expandable);
+  border-right: solid 1px var(--expandable);
+`;
+
+const Details = styled.button`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--label);
+`;
+
+const Button = styled.button`
+  width: 1rem;
+`;
+
 const DocumentFooter = () => {
-  return <StyledDocumentFooter></StyledDocumentFooter>;
+  return (
+    <StyledDocumentFooter>
+      <Zoom>100%</Zoom>
+      <DetailsContainer>
+        <Details>700 px x 500 px (300 ppi)</Details>
+        <Button>F</Button>
+      </DetailsContainer>
+    </StyledDocumentFooter>
+  );
 };
 
 export default DocumentFooter;
