b80fab1a56b73f4ab0568de11737426935beafaa
Chase Manning <me@chasemanning.co.nz>
2021-01-18 00:17:52 +1300

    :bug: fix error with window not found

--- src/sections/windows/Window.tsx
+++ src/sections/windows/Window.tsx
@@ -8,6 +8,22 @@ const Window = (props: Props) => {
   switch (props.window) {
     case "Color":
       return <p>meow</p>;
+    case "Swatches":
+      return <p>meow</p>;
+    case "Gradients":
+      return <p>meow</p>;
+    case "Patterns":
+      return <p>meow</p>;
+    case "Properties":
+      return <p>meow</p>;
+    case "Adjustments":
+      return <p>meow</p>;
+    case "Layers":
+      return <p>meow</p>;
+    case "Channels":
+      return <p>meow</p>;
+    case "Paths":
+      return <p>meow</p>;
     default:
       throw new Error("Window Not Found");
   }
