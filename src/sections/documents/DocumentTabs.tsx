a744a9fe19cbbdb46613cf03ebc2b99442eb3300
Chase Manning <me@chasemanning.co.nz>
2021-01-17 15:59:02 +1300

    :sparkles: create document tabs component

---
+++ src/sections/documents/DocumentTabs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDocumentTabs = styled.div`
+  width: 100%;
+  height: 4rem;
+  background-color: var(--expandable);
+  border-bottom: solid 1px var(--border);
+`;
+
+const DocumentTabs = () => {
+  return <StyledDocumentTabs></StyledDocumentTabs>;
+};
+
+export default DocumentTabs;
