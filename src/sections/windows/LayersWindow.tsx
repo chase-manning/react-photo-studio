de7db9e1586c741b702639b9f851d8eb59460940
Chase Manning <me@chasemanning.co.nz>
2021-01-18 16:22:43 +1300

    :rocket: add layers to layer window

--- src/sections/windows/LayersWindow.tsx
+++ src/sections/windows/LayersWindow.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { LayerType, selectLayers } from "../../state/layersSlice";
 
 const StyledLayersWindow = styled.div`
   width: 100%;
   height: 100%;
 `;
 
+const Layer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
 const LayersWindow = () => {
-  return <StyledLayersWindow></StyledLayersWindow>;
+  const layers = useSelector(selectLayers);
+
+  return (
+    <StyledLayersWindow>
+      {layers.map((layer: LayerType) => (
+        <Layer>{layer.name}</Layer>
+      ))}
+    </StyledLayersWindow>
+  );
 };
 
 export default LayersWindow;
