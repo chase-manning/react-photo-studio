d2121b8ce63f12f3ebf93a6e21ce790fc7e8b1f8
Chase Manning <me@chasemanning.co.nz>
2021-01-18 14:16:33 +1300

    :sparkles: create layers window component

---
+++ src/sections/windows/LayersWindow.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLayersWindow = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const LayersWindow = () => {
+  return <StyledLayersWindow></StyledLayersWindow>;
+};
+
+export default LayersWindow;
