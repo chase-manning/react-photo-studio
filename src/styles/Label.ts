69a97f791d690f6f7629014746d217c9bb4b6c1a
Chase Manning <me@chasemanning.co.nz>
2021-01-23 19:44:32 +1300

    :sparkles: create label styled component

---
+++ src/styles/Label.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+type Props = {
+  scrubCursor?: boolean;
+};
+
+const Label = styled.div`
+  color: var(--label);
+  margin-right: 0.4rem;
+  user-select: none;
+  cursor: ${(props: Props) =>
+    props.scrubCursor ? "url('cursors/scrub.png'), auto" : "auto"};
+`;
+
+export default Label;
