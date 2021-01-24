902e1d88d4834884ad1bad9470e995be91e2bdd0
Chase Manning <me@chasemanning.co.nz>
2021-01-24 13:30:51 +1300

    :art: change layer window to use layer

--- src/sections/windows/LayersWindow.tsx
+++ src/sections/windows/LayersWindow.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { LayerType, selectLayers } from "../../state/layersSlice";
-import eye from "../../assets/svgs/layers/eye.svg";
-import lock from "../../assets/svgs/layers/lock.svg";
 import LayersFooter from "./LayersFooter";
 import LayerFilters from "./LayerFilters";
 import LayerBlending from "./LayerBlending";
 import LayerLock from "./LayerLock";
+import Layer from "./Layer";
 
 const StyledLayersWindow = styled.div`
   width: 100%;
@@ -26,54 +25,6 @@ const Layers = styled.div`
   background-color: var(--layer-bg);
 `;
 
-const Layer = styled.div`
-  width: 100%;
-  height: 3.8rem;
-  display: flex;
-  border-bottom: solid 1px var(--hover-bg);
-  background-color: var(--panel);
-  align-items: center;
-`;
-
-const Visibility = styled.div`
-  height: 100%;
-  width: 3.5rem;
-  border-right: solid 1px var(--hover-bg);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Eye = styled.img`
-  height: 0.9rem;
-`;
-
-const Canvas = styled.div`
-  height: 100%;
-  width: 3.8rem;
-  background-color: white;
-  border: solid 1px var(--layer-border);
-`;
-
-const Content = styled.div`
-  flex: 1;
-  height: 100%;
-  padding: 0.4rem;
-  display: flex;
-  align-items: center;
-`;
-
-const LayerName = styled.div`
-  color: var(--layer-text);
-  margin-left: 0.7rem;
-`;
-
-const Lock = styled.img`
-  height: 1.2rem;
-  margin-right: 1.5rem;
-  transform: translateY(-0.1rem);
-`;
-
 const LayersWindow = () => {
   const layers = useSelector(selectLayers);
 
@@ -84,16 +35,7 @@ const LayersWindow = () => {
       <LayerLock />
       <Layers>
         {layers.map((layer: LayerType) => (
-          <Layer>
-            <Visibility>
-              <Eye src={eye} />
-            </Visibility>
-            <Content>
-              <Canvas />
-              <LayerName>{layer.name}</LayerName>
-            </Content>
-            {layer.locked && <Lock src={lock} />}
-          </Layer>
+          <Layer layer={layer} />
         ))}
       </Layers>
       <LayersFooter />
