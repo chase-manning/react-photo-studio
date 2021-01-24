55a10526edf4f281509174339964b3919a1909f1
Chase Manning <me@chasemanning.co.nz>
2021-01-24 20:39:17 +1300

    :sparkles: create disabled styled component

---
+++ src/styles/Disabled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Disable = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: var(--panel);
+  opacity: 0.6;
+`;
+
+export default Disable;
