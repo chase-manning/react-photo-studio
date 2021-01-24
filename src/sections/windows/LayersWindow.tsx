24e8d0287471e3f5305bbcdce236ac746b2a03ed
Chase Manning <me@chasemanning.co.nz>
2021-01-24 13:28:06 +1300

    :art: change layers window to use layer lock

--- src/sections/windows/LayersWindow.tsx
+++ src/sections/windows/LayersWindow.tsx
@@ -4,17 +4,10 @@ import styled from "styled-components";
 import { LayerType, selectLayers } from "../../state/layersSlice";
 import eye from "../../assets/svgs/layers/eye.svg";
 import lock from "../../assets/svgs/layers/lock.svg";
-import PercentSelector from "../../shared/PercentSelector";
-import Button from "../../styles/Button";
-import Label from "../../styles/Label";
-
-import transparent from "../../assets/pngs/transparent.png";
-import brush from "../../assets/svgs/tools/brush.svg";
-import move from "../../assets/svgs/tools/move.svg";
-import artboardNesting from "../../assets/pngs/artboard-nesting.png";
 import LayersFooter from "./LayersFooter";
 import LayerFilters from "./LayerFilters";
 import LayerBlending from "./LayerBlending";
+import LayerLock from "./LayerLock";
 
 const StyledLayersWindow = styled.div`
   width: 100%;
@@ -23,31 +16,6 @@ const StyledLayersWindow = styled.div`
   flex-direction: column;
   justify-content: space-between;
   padding-top: 0.5rem;
-`;
-
-const Setting = styled.div`
-  width: 100%;
-  height: 2.8rem;
-  display: flex;
-  border-bottom: solid 1px var(--section-line);
-  background-color: var(--panel);
-  align-items: center;
-  padding: 0 0.3rem;
-`;
-
-const ButtonAsset = styled.img`
-  height: 80%;
-`;
-
-const LockButtons = styled.div`
-  height: 95%;
-  display: flex;
-  margin-right: 0.9rem;
-`;
-
-const SpacingSmall = styled.div`
-  height: 100%;
-  width: 0.2rem;
 `;
 
 const Layers = styled.div`
@@ -113,28 +81,7 @@ const LayersWindow = () => {
     <StyledLayersWindow>
       <LayerFilters />
       <LayerBlending />
-      <Setting>
-        <SpacingSmall />
-        <Label>Lock:</Label>
-        <LockButtons>
-          <Button selected={false}>
-            <ButtonAsset src={transparent} />
-          </Button>
-          <Button selected={false}>
-            <ButtonAsset src={brush} />
-          </Button>
-          <Button selected={false}>
-            <ButtonAsset src={move} />
-          </Button>
-          <Button selected={false}>
-            <ButtonAsset src={artboardNesting} />
-          </Button>
-          <Button selected={false}>
-            <ButtonAsset src={lock} />
-          </Button>
-        </LockButtons>
-        <PercentSelector label={"Fill"} value={100}></PercentSelector>
-      </Setting>
+      <LayerLock />
       <Layers>
         {layers.map((layer: LayerType) => (
           <Layer>
