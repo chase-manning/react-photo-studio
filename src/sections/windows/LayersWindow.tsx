0ec6b6830786efa5dc4d1d51171fcbe44c517221
Chase Manning <me@chasemanning.co.nz>
2021-01-24 13:33:31 +1300

    :rocket: add layers scroll bar

--- src/sections/windows/LayersWindow.tsx
+++ src/sections/windows/LayersWindow.tsx
@@ -17,12 +17,24 @@ const StyledLayersWindow = styled.div`
   padding-top: 0.5rem;
 `;
 
-const Layers = styled.div`
+const LayersContainer = styled.div`
   width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+const Layers = styled.div`
+  flex: 1;
   height: 100%;
   display: flex;
   flex-direction: column;
   background-color: var(--layer-bg);
+`;
+
+const ScrollBar = styled.div`
+  height: 100%;
+  width: 3rem;
+  background-color: var(--sidebar);
 `;
 
 const LayersWindow = () => {
@@ -33,11 +45,14 @@ const LayersWindow = () => {
       <LayerFilters />
       <LayerBlending />
       <LayerLock />
-      <Layers>
-        {layers.map((layer: LayerType) => (
-          <Layer layer={layer} />
-        ))}
-      </Layers>
+      <LayersContainer>
+        <Layers>
+          {layers.map((layer: LayerType) => (
+            <Layer layer={layer} />
+          ))}
+        </Layers>
+        <ScrollBar />
+      </LayersContainer>
       <LayersFooter />
     </StyledLayersWindow>
   );
