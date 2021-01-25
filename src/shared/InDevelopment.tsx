04d35bd6e2c226d9e8682f2d00e162c8e827bd1e
Chase Manning <me@chasemanning.co.nz>
2021-01-25 22:11:52 +1300

    :art: switch to saga feature tracking

---
+++ src/shared/InDevelopment.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { selectedFeatureRequesting } from "../state/featureSlice";
+
+type Props = {
+  show: boolean;
+};
+
+const StyledInDevelopment = styled.div`
+  position: fixed;
+  left: 50%;
+  bottom: 0;
+  transform: ${(props: Props) =>
+    props.show ? "translate(-50%, 0)" : "translate(-50%, -110%)"};
+  width: 30%;
+  height: 20rem;
+`;
+
+const InDevelopment = () => {
+  const requesting = useSelector(selectedFeatureRequesting);
+
+  return <StyledInDevelopment show={!!requesting}>meow</StyledInDevelopment>;
+};
+
+export default InDevelopment;
