1ba7e3dc8aedd6b281fafac6fc8d64efea7e2af5
Chase Manning <me@chasemanning.co.nz>
2021-01-26 20:55:47 +1300

    :rocket: setup pixi.js as canvas

--- src/sections/documents/Canvas.tsx
+++ src/sections/documents/Canvas.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
+import { Application } from "pixi.js";
 
-const StyledCanvas = styled.div`
-  height: 50rem;
-  width: 70rem;
-  background-color: white;
-`;
+const StyledCanvas = styled.div``;
+
+const app = new Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x5bba6f,
+});
 
 const Canvas = () => {
-  return <StyledCanvas></StyledCanvas>;
+  const canvas = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (canvas.current) canvas.current.appendChild(app.view);
+    app.start();
+    return () => app.destroy(true);
+  }, []);
+
+  return <StyledCanvas ref={canvas} />;
 };
 
 export default Canvas;
