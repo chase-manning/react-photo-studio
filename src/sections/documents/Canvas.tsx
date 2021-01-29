c6551ac63cf720e088b0d8035c1f14ddcf296ebc
Chase Manning <me@chasemanning.co.nz>
2021-01-29 22:56:43 +1300

    :rocket: add circle to canvas

--- src/sections/documents/Canvas.tsx
+++ src/sections/documents/Canvas.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
-import { Application } from "pixi.js";
+import * as PIXI from "pixi.js";
 
-const StyledCanvas = styled.div``;
+const StyledCanvas = styled.div`
+  width: 900px;
+  height: 800px;
+`;
 
-const app = new Application({
+const app = new PIXI.Application({
   width: 900,
   height: 600,
   backgroundColor: 0xffffff,
+  antialias: true,
+  resolution: 1,
 });
 
 const Canvas = () => {
@@ -16,6 +21,15 @@ const Canvas = () => {
   useEffect(() => {
     if (canvas.current) canvas.current.appendChild(app.view);
     app.start();
+
+    const container = new PIXI.Container();
+    app.stage.addChild(container);
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(0xe74c3c);
+    graphics.drawCircle(60, 185, 40);
+    graphics.endFill();
+    container.addChild(graphics);
+
     return () => app.destroy(true);
   }, []);
 
