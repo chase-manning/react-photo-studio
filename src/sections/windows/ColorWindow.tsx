1341abea8faadf596a4825b0aaa3190becfb72e2
Chase Manning <me@chasemanning.co.nz>
2021-01-31 20:49:54 +1300

    :sparkles: create color window component

---
+++ src/sections/windows/ColorWindow.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledColorWindow = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const ColorWindow = () => {
+  return <StyledColorWindow>meow</StyledColorWindow>;
+};
+
+export default ColorWindow;
