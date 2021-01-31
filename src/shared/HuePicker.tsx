04b79f332608e0f90cc177edecc55c122544267e
Chase Manning <me@chasemanning.co.nz>
2021-01-31 21:12:09 +1300

    :sparkles: create hue picker component

---
+++ src/shared/HuePicker.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHuePicker = styled.div`
+  height: 100%;
+  width: 2.8rem;
+  background: linear-gradient(
+    #ff0000,
+    #ff00ff,
+    #0000ff,
+    #00ffff,
+    #00ff00,
+    #ffff00,
+    #ff0000
+  );
+  margin-left: 2rem;
+`;
+
+const HuePicker = () => {
+  return <StyledHuePicker></StyledHuePicker>;
+};
+
+export default HuePicker;
