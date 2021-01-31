f311fd689de164c354f3cde44c6d1b0ef6777ee7
Chase Manning <me@chasemanning.co.nz>
2021-01-31 16:31:30 +1300

    :fire: remove redundant import

--- src/sections/documents/Cursor.tsx
+++ src/sections/documents/Cursor.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import {
-  Position,
   selectCursorDocument,
   selectCursorPosition,
 } from "../../state/cursorSlice";
