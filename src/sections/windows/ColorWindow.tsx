817f2e3132a070ac22b5c6dc8157ee9b41e5fbb6
Chase Manning <me@chasemanning.co.nz>
2021-01-31 21:00:14 +1300

    :rocket: add colors to color window

--- src/sections/windows/ColorWindow.tsx
+++ src/sections/windows/ColorWindow.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import ColorPicker from "../../shared/ColorPicker";
+import Colors from "../tools/Colors";
 
 const StyledColorWindow = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+`;
+
+const ColorContainer = styled.div`
+  width: 4.6rem;
 `;
 
 const ColorWindow = () => {
   return (
     <StyledColorWindow>
+      <ColorContainer>
+        <Colors showButtons={false} />
+      </ColorContainer>
       <ColorPicker />
     </StyledColorWindow>
   );
