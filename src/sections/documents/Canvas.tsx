6dfc12af433568ac11c025b844d9c8a313748acc
Chase Manning <me@chasemanning.co.nz>
2021-01-31 14:20:28 +1300

    :rocket: add dynamic canvas generation

--- src/sections/documents/Canvas.tsx
+++ src/sections/documents/Canvas.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import * as PIXI from "pixi.js";
+import { useSelector } from "react-redux";
+import { selectEvents, Event, EventType } from "../../state/fileSlice";
 
 const StyledCanvas = styled.div`
   width: 900px;
-  height: 800px;
+  height: 600px;
 `;
 
 const app = new PIXI.Application({
@@ -15,27 +17,38 @@ const app = new PIXI.Application({
   resolution: 1,
 });
 
-const Circle = (layer: PIXI.Container, x: number, y: number, size: number) => {
-  var graphics = new PIXI.Graphics();
-  graphics.beginFill(0xe74c3c);
+const Circle = (
+  graphics: PIXI.Graphics,
+  x: number,
+  y: number,
+  size: number,
+  color?: number
+) => {
+  graphics.beginFill(color);
   graphics.drawCircle(x, y, size);
   graphics.endFill();
-  layer.addChild(graphics);
 };
 
 const Canvas = () => {
   const canvas = useRef<HTMLDivElement>(null);
+  const events = useSelector(selectEvents);
 
   useEffect(() => {
     if (canvas.current) canvas.current.appendChild(app.view);
     app.start();
-    const layer = new PIXI.Container();
-    app.stage.addChild(layer);
-
-    Circle(layer, 100, 100, 80);
 
     return () => app.destroy(true);
   }, []);
+
+  app.stage.removeChildren();
+  const layer = new PIXI.Container();
+  app.stage.addChild(layer);
+  var graphics = new PIXI.Graphics();
+  events.forEach((event: Event) => {
+    if (event.type === EventType.CIRCLE)
+      Circle(graphics, event.x!, event.y!, event.size!, event.color);
+  });
+  layer.addChild(graphics);
 
   return <StyledCanvas ref={canvas} />;
 };
