2a9912084494fd554186cf3ec9c863965ca801f7
Chase Manning <me@chasemanning.co.nz>
2021-01-31 16:48:30 +1300

    :rotating_light: remove redundant imports

--- src/sections/documents/Document.tsx
+++ src/sections/documents/Document.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { ToolOption } from "../../state/toolSchema";
