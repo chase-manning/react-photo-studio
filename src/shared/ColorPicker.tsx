116f322f3439a883c496675079d0bec84fb749ed
Chase Manning <me@chasemanning.co.nz>
2021-01-31 20:52:04 +1300

    :sparkles: create color picker component

---
+++ src/shared/ColorPicker.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledColorPicker = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const ColorPicker = () => {
+  return <StyledColorPicker>meow</StyledColorPicker>;
+};
+
+export default ColorPicker;
