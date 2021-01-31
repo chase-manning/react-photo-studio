8a5d524e62fa0e793d754df8ed98bfac96afda4d
Chase Manning <me@chasemanning.co.nz>
2021-01-31 20:55:03 +1300

    :rocket: add gradient to color picker

--- src/shared/ColorPicker.tsx
+++ src/shared/ColorPicker.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledColorPicker = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
+  background: linear-gradient(90deg, white, #00ff44);
+`;
+
+const DarkOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), black);
 `;
 
 const ColorPicker = () => {
-  return <StyledColorPicker>meow</StyledColorPicker>;
+  return (
+    <StyledColorPicker>
+      <DarkOverlay />
+    </StyledColorPicker>
+  );
 };
 
 export default ColorPicker;
