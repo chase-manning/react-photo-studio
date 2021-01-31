9d4d0f50193d0cdcb50de538e065bc0a4fe59ee8
Chase Manning <me@chasemanning.co.nz>
2021-01-31 16:37:35 +1300

    :sparkles: add canvas position to cursor slice

--- src/state/cursorSlice.ts
+++ src/state/cursorSlice.ts
@@ -10,6 +10,7 @@ export interface CursorSlice {
   position: Position;
   document: boolean;
   down: boolean;
+  canvasPosition: Position;
 }
 
 const initialState: CursorSlice = {
@@ -19,6 +20,10 @@ const initialState: CursorSlice = {
   },
   document: false,
   down: false,
+  canvasPosition: {
+    x: 0,
+    y: 0,
+  },
 };
 
 export const cursorSlice = createSlice({
@@ -40,6 +45,9 @@ export const cursorSlice = createSlice({
     mouseUp: (state) => {
       state.down = false;
     },
+    setCanvasPosition: (state, action: PayloadAction<Position>) => {
+      state.canvasPosition = action.payload;
+    },
   },
 });
 
@@ -49,10 +57,17 @@ export const {
   exitDocument,
   mouseDown,
   mouseUp,
+  setCanvasPosition,
 } = cursorSlice.actions;
 
 export const selectCursorPosition = (state: RootState) => state.cursor.position;
 export const selectCursorDocument = (state: RootState) => state.cursor.document;
 export const selectCursorDown = (state: RootState) => state.cursor.down;
+export const selectCursorCanvasPosition = (state: RootState) => {
+  return {
+    x: state.cursor.position.x - state.cursor.canvasPosition.x,
+    y: state.cursor.position.y - state.cursor.canvasPosition.y,
+  };
+};
 
 export default cursorSlice.reducer;
