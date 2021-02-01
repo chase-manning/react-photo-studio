44979c958102d0d754c7e4280b190fc4b3ffca22
Chase Manning <me@chasemanning.co.nz>
2021-02-01 21:36:44 +1300

    :lipstick: decrease width of tab underline

--- src/popups/NewFileTab.tsx
+++ src/popups/NewFileTab.tsx
@@ -8,7 +8,7 @@ type TabProps = {
 const Tab = styled.button`
   position: relative;
   font-size: 1.4rem;
-  padding: 2rem 1rem;
+  margin: 2rem 1rem;
   cursor: pointer;
 
   color: ${(props: TabProps) =>
@@ -22,7 +22,7 @@ const Underline = styled.div`
   position: absolute;
   left: 0;
   width: 100%;
-  bottom: -2px;
+  bottom: -2rem;
   height: 2px;
   background-color: var(--white);
   border-radius: 1px;
