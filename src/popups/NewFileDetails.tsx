7261de747bf07f8bd3d4e91b731e0da78b498a28
Chase Manning <me@chasemanning.co.nz>
2021-02-01 22:08:06 +1300

    :sparkles: create new file details component

---
+++ src/popups/NewFileDetails.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Details = styled.div`
+  height: 30rem;
+  width: 10rem;
+`;
+
+const NewFileDetails = () => {
+  return <Details></Details>;
+};
+
+export default NewFileDetails;
