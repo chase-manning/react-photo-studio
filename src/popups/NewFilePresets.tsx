3b5ce9a3779eea056aecd18cb322fb959ae4309c
Chase Manning <me@chasemanning.co.nz>
2021-02-01 22:03:35 +1300

    :sparkles: create new file presets component

---
+++ src/popups/NewFilePresets.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Presets = styled.div`
+  width: 20rem;
+  height: 100%;
+`;
+
+const NewFilePresets = () => {
+  return <Presets>meow</Presets>;
+};
+
+export default NewFilePresets;
