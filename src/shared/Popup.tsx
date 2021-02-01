7bdf891e319403af1057a8ebb72803b48d596802
Chase Manning <me@chasemanning.co.nz>
2021-02-01 18:19:29 +1300

    :sparkles: create popup component

---
+++ src/shared/Popup.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPopup = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Window = styled.div`
+  width: 50%;
+  height: 50%;
+  background-color: var(--popup-new-bg);
+  border-radius: 10px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 3rem;
+  background-color: var(--popup-new-header);
+`;
+
+const Content = styled.div`
+  width: 100%;
+  flex: 1;
+`;
+
+const Popup = () => {
+  return (
+    <StyledPopup>
+      <Window>
+        <Header></Header>
+        <Content></Content>
+      </Window>
+    </StyledPopup>
+  );
+};
+
+export default Popup;
