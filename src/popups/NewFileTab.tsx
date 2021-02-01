c9bb08930c317ffcb3981e0faa461da34aea2201
Chase Manning <me@chasemanning.co.nz>
2021-02-01 21:31:48 +1300

    :sparkles: create new file tab component

---
+++ src/popups/NewFileTab.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+type TabProps = {
+  active: boolean;
+};
+
+const Tab = styled.div`
+  position: relative;
+  color: ${(props: TabProps) =>
+    props.active ? "var(--white)" : "var(--new-file-tab-text)"};
+  font-size: 1.2rem;
+  padding: 2rem 1rem;
+`;
+
+const Underline = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  bottom: -2px;
+  height: 2px;
+  background-color: var(--white);
+  border-radius: 1px;
+`;
+
+export type NewFileTabType = {
+  label: string;
+  active: boolean;
+};
+
+type Props = {
+  tab: NewFileTabType;
+};
+
+const NewFileTab = (props: Props) => {
+  return (
+    <Tab active={props.tab.active}>
+      {props.tab.label}
+      {props.tab.active && <Underline />}
+    </Tab>
+  );
+};
+
+export default NewFileTab;
