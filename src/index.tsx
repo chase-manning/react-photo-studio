003411f18e565061ac874dc7396ff4f526aff644
Chase Manning <me@chasemanning.co.nz>
2022-01-02 19:56:53 +0000

    :fire: remove report web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import GlobalStyles from "./styles/GlobalStyles";
 import { store } from "./state/store";
 import { Provider } from "react-redux";
@@ -17,5 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals();
