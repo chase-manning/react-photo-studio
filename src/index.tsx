c8bb831327d6283f0834881235b75d9b1345f8d5
Chase Manning <me@chasemanning.co.nz>
2022-01-02 22:13:26 +0000

    :rotating_light: fix lint warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import App from "./App";
 import GlobalStyles from "./styles/GlobalStyles";
 import { store } from "./state/store";
-import { Provider } from "react-redux";
 import Shortcuts from "./orchestration/Shortcuts";
 
 ReactDOM.render(
