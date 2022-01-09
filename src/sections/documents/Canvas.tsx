811b813445b99a56aeefcc2011046ecba15bad2c
Chase Manning <me@chasemanning.co.nz>
2022-01-09 18:03:16 +0000

    :bug: fix bug with sharp line edges

--- src/sections/documents/Canvas.tsx
+++ src/sections/documents/Canvas.tsx
@@ -61,6 +61,8 @@ const Canvas = () => {
     // Drawing Line
     if (event.type === "line") {
       if (!event.points || event.points.length === 0) return;
+
+      // Starting with a circle
       if (event.points.length === 1) {
         const circle = new PIXI.Graphics();
         Circle(
@@ -71,7 +73,10 @@ const Canvas = () => {
           event.color
         );
         layer.addChild(circle);
-      } else {
+      }
+
+      // Starting the line
+      else {
         const line = new PIXI.Graphics();
         line.lineTextureStyle({
           width: event.size,
@@ -80,11 +85,19 @@ const Canvas = () => {
           alpha: 1,
           join: PIXI.LINE_JOIN.ROUND,
           cap: PIXI.LINE_CAP.ROUND,
-          miterLimit: 198,
+          miterLimit: 1000,
         });
         line.moveTo(event.points[0].x, event.points[0].y);
+
+        // Adding all steps of the line
         event.points.forEach((point: Position, index: number) => {
           if (index === 0) return;
+
+          // Handling sharp edges by adding a circle
+          const circle = new PIXI.Graphics();
+          Circle(circle, point.x, point.y, (event.size || 40) / 2, event.color);
+          layer.addChild(circle);
+
           line.lineTo(point.x, point.y);
         });
         layer.addChild(line);
