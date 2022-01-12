e6ba9962ee5f143dbed5b2281301e9067360793b
Chase Manning <me@chasemanning.co.nz>
2022-01-12 23:02:53 +0000

    :rocket: add recent recommendation

--- src/popups/NewFilePresets.tsx
+++ src/popups/NewFilePresets.tsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
+import RecommendationBanner from "./RecommendationBanner";
 
 const Presets = styled.div`
   width: 80rem;
   background-color: var(--popup-inner-bg);
+  padding: 1.2rem 1.3rem;
 `;
 
 const NewFilePresets = () => {
-  return <Presets>meow</Presets>;
+  return (
+    <Presets>
+      <RecommendationBanner />
+    </Presets>
+  );
 };
 
 export default NewFilePresets;
